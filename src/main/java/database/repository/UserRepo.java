8c91713c1fb5aeb875a464a7c6542324f1537c5b
joelpoirier1 <43867550+joelpoirier1@users.noreply.github.com>
2020-03-10 14:05:05 -0600

    style

--- src/main/java/database/repository/UserRepo.java
+++ src/main/java/database/repository/UserRepo.java
@@ -64,10 +64,10 @@ public class UserRepo implements InterfaceUserDatabase
         UserRepo db = new UserRepo();
 
         //populate DB upon startup... comment out after db is created and delete when db is running on a server...
-        /*db.addUser(new UserModel("alyssa", "lee"));
-        db.addUser(new UserModel("admin", "admin"));
-        db.addUser(new UserModel("username", "password"));*/
+        /*db.addUser(new User("alyssa", "lee"));
+        db.addUser(new User("admin", "admin"));
+        db.addUser(new User("username", "password"));
 
-        //System.out.println(db.validateUsernameExistence("alyssa"));
+        System.out.println(db.validateUsernameExistence("alyssa"));*/
     }
 }
