740c16355075c6566920a45a51489ce87aaae88e
kacper <kporebski15@gmail.com>
2020-03-11 22:59:46 -0600

    https get request

--- src/main/java/model/Comment.java
+++ src/main/java/model/Comment.java
@@ -80,6 +80,9 @@ public class Comment {
     public String getComment() {         return comment;     }
     public void setComment(String comment) {        this.comment = comment;    }
 
+    public String getDate() {         return date;     }
+    public void setDate(String date) {         this.date = date;     }
+
     @Override
     public String toString() {
         return "Comment{" +
