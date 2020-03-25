75f199a47659ec69f3e62d82e86f6c22cf592936
Julia Grab <julia.grab@ucalgary.ca>
2020-03-24 18:26:44 -0600

    Added homepage functionalities

--- src/main/java/com/seng401/mango/HomeController.java
+++ src/main/java/com/seng401/mango/HomeController.java
@@ -8,28 +8,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.UUID;
 
 @Controller
 public class HomeController {
 
     private CommentRequest request = new CommentRequest();
-    @RequestMapping(value="/add", method = RequestMethod.POST)
-    public String addPost() {
-        return "post";
-    }
 
     @RequestMapping(value="/addComment", method = RequestMethod.POST)
-    public String addComment(@ModelAttribute("homeForm") HomeForm homeForm, Model model) {
+    public String addComment(@ModelAttribute("homeForm") CommentForm commentForm, Model model) {
 
             //homeForm.getParentID();
 
+        Comment newComment = new Comment(commentForm.getComment());
 //        Comment comment = new Comment(id);
-        model.addAttribute("myPost", homeForm.getParentID().toString());
+        model.addAttribute("myPost", commentForm.getParentID().toString());
 
        // model.addAttribute("myPost", request.getCommentByCommentID(UUID.fromString(id)));
-        return "comment";
+        return "home";
+    }
+
+    @RequestMapping(value="/addPost", method = RequestMethod.POST)
+    public String addPost(@ModelAttribute("postForm") PostForm postForm, Model model) {
+
+        //Create comment
+
+        Comment newComment = new Comment(postForm.getComment());
+        model.addAttribute("posts", request.getAllComments().getComments());
+
+        return "home";
     }
 }
