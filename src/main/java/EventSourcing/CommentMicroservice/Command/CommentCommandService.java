90c0d2ff50dfee6b92717734a686f272e1799103
Julia Grab <julia.grab@ucalgary.ca>
2020-03-26 00:15:26 -0600

    Added homepage functionalities

--- src/main/java/EventSourcing/CommentMicroservice/Command/CommentCommandService.java
+++ src/main/java/EventSourcing/CommentMicroservice/Command/CommentCommandService.java
@@ -1,10 +1,10 @@
-package EventSourcing.CommentMicroservice.Command;
-
-import EventSourcing.BasicClasses.Comment;
-
-import java.util.concurrent.CompletableFuture;
-
-/*Service Layer component to handle the Commands with respect to the comments*/
-public interface CommentCommandService {
-      CompletableFuture<String> createComment(Comment comment);
-}
+//package EventSourcing.CommentMicroservice.Command;
+//
+//import EventSourcing.BasicClasses.Comment;
+//
+//import java.util.concurrent.CompletableFuture;
+//
+///*Service Layer component to handle the Commands with respect to the comments*/
+//public interface CommentCommandService {
+//      CompletableFuture<String> createComment(Comment comment);
+//}
--- src/main/java/EventSourcing/CommentMicroservice/Query/CommentQueryServiceImpl.java
+++ src/main/java/EventSourcing/CommentMicroservice/Query/CommentQueryServiceImpl.java
@@ -1,50 +1,50 @@
-package EventSourcing.CommentMicroservice.Query;
-
-import EventSourcing.BasicClasses.Comment;
-import EventSourcing.BasicClasses.CommentList;
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-@Service //This has been added back in
-/*Not Mandatory for Event Sourcing using Axon - used for testing purpose*/
-public class CommentQueryServiceImpl implements CommentQueryService {
-    /*Axon EventStore to provide a method to read events for a particular
-    * Aggregate Id*/
-    private final EventStore eventStore;
-
-    //This constructor is called immediately when the application is run
-    public CommentQueryServiceImpl(EventStore eventStore) {
-        System.out.println("Inside Constructor CommentQueryServiceImpl(EventStore eventStore)");
-        this.eventStore = eventStore;
-    }
-
-    //Returns the serialized version of the comment. Meaning, turns a sequence of bytes that holds comment info
-    @Override
-    public List<Object> getCommentBytesFromEventStore(UUID commentID) {
-        System.out.println("Inside getCommentBytesFromEventStore(UUID commentID)");
-        String commentStringID = commentID.toString();
-        List<Object> aCommentEvent = eventStore.readEvents(commentStringID).asStream().map(s -> s.getPayload()).collect(Collectors.toList());
-        return aCommentEvent;
-    }
-
-    //Returns the message of a comment.
-    public Comment getCommentCreatedEventClassFromEventStore(UUID commentID) {
-        System.out.println("Inside getCommentMessageFromEventStore(UUID commentID)");
-        Comment comment = (Comment) getCommentBytesFromEventStore(commentID).get(0);
-        return comment;
-    }
-
-    public CommentList getAllCommentsFromEventStore(){
-        CommentList commentList = new CommentList();
-//        for (int i = 0; i < numberOfCommentsInCommentIDDataBase ; i++){
-//            UUID commentID = UUID.fromString(getCommentIDFromDataBase(i));
-//            Comment comment = getCommentCreatedEventClassFromEventStore(commentID);
-//            Comment comment = new Comment(comment.getCommentID(), comment.getParentD(), comment.getMessage());
-//        }
-        return commentList;
-    }
-}
+//package EventSourcing.CommentMicroservice.Query;
+//
+//import EventSourcing.BasicClasses.Comment;
+//import EventSourcing.BasicClasses.CommentList;
+//import org.axonframework.eventsourcing.eventstore.EventStore;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.UUID;
+//import java.util.stream.Collectors;
+//
+//@Service //This has been added back in
+///*Not Mandatory for Event Sourcing using Axon - used for testing purpose*/
+//public class CommentQueryServiceImpl implements CommentQueryService {
+//    /*Axon EventStore to provide a method to read events for a particular
+//    * Aggregate Id*/
+//    private final EventStore eventStore;
+//
+//    //This constructor is called immediately when the application is run
+//    public CommentQueryServiceImpl(EventStore eventStore) {
+//        System.out.println("Inside Constructor CommentQueryServiceImpl(EventStore eventStore)");
+//        this.eventStore = eventStore;
+//    }
+//
+//    //Returns the serialized version of the comment. Meaning, turns a sequence of bytes that holds comment info
+//    @Override
+//    public List<Object> getCommentBytesFromEventStore(UUID commentID) {
+//        System.out.println("Inside getCommentBytesFromEventStore(UUID commentID)");
+//        String commentStringID = commentID.toString();
+//        List<Object> aCommentEvent = eventStore.readEvents(commentStringID).asStream().map(s -> s.getPayload()).collect(Collectors.toList());
+//        return aCommentEvent;
+//    }
+//
+//    //Returns the message of a comment.
+//    public Comment getCommentCreatedEventClassFromEventStore(UUID commentID) {
+//        System.out.println("Inside getCommentMessageFromEventStore(UUID commentID)");
+//        Comment comment = (Comment) getCommentBytesFromEventStore(commentID).get(0);
+//        return comment;
+//    }
+//
+//    public CommentList getAllCommentsFromEventStore(){
+//        CommentList commentList = new CommentList();
+////        for (int i = 0; i < numberOfCommentsInCommentIDDataBase ; i++){
+////            UUID commentID = UUID.fromString(getCommentIDFromDataBase(i));
+////            Comment comment = getCommentCreatedEventClassFromEventStore(commentID);
+////            Comment comment = new Comment(comment.getCommentID(), comment.getParentD(), comment.getMessage());
+////        }
+//        return commentList;
+//    }
+//}
