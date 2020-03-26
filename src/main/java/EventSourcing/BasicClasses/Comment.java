7700436a1c6cd683eda9ff4fcbe996704bed6002
Julia Grab <julia.grab@ucalgary.ca>
2020-03-26 12:44:06 -0600

    Adding event sourcing

--- src/main/java/EventSourcing/BasicClasses/Comment.java
+++ src/main/java/EventSourcing/BasicClasses/Comment.java
@@ -1,45 +1,45 @@
-package EventSourcing.BasicClasses;
-
-import java.util.UUID;
-
-public class Comment {
-    private UUID commentID;
-    private UUID parentID;
-    private UUID userID;
-    private String message;
-
-    //Empty constructor
-    public Comment(){};
-
-    //Constructor
-    public Comment(UUID commentID, UUID parentID, String message) {
-        this.commentID = commentID;
-        this.parentID = parentID;
-        this.userID = null;  /*TODO: Change this later*/
-        this.message = message;
-
-    }
-
-    //Getters and setters
-    public UUID getCommentID()  { return commentID; }
-    public void setCommentID(UUID commentID)  { this.commentID = commentID; }
-
-    public UUID getParentID()  { return parentID; }
-    public void getParentID(UUID parentID)  { this.parentID = parentID; }
-
-    public String getMessage()  { return message; }
-    public void setMessage(String message)  { this.message = message; }
-
-    public UUID getUserID() { return userID;  }
-    public void setUserID(UUID userID) { this.userID = userID; }
-
-    @Override
-    public String toString() {
-        return "Comment{" +
-                ", commentID=" + commentID +
-                ", postID=" + parentID +
-                ", userID=" + userID +
-                ", comment='" + message + '\'' +
-                '}';
-    }
-}
+//package EventSourcing.BasicClasses;
+//
+//import java.util.UUID;
+//
+//public class Comment {
+//    private UUID commentID;
+//    private UUID parentID;
+//    private UUID userID;
+//    private String message;
+//
+//    //Empty constructor
+//    public Comment(){};
+//
+//    //Constructor
+//    public Comment(UUID commentID, UUID parentID, String message) {
+//        this.commentID = commentID;
+//        this.parentID = parentID;
+//        this.userID = null;  /*TODO: Change this later*/
+//        this.message = message;
+//
+//    }
+//
+//    //Getters and setters
+//    public UUID getCommentID()  { return commentID; }
+//    public void setCommentID(UUID commentID)  { this.commentID = commentID; }
+//
+//    public UUID getParentID()  { return parentID; }
+//    public void getParentID(UUID parentID)  { this.parentID = parentID; }
+//
+//    public String getMessage()  { return message; }
+//    public void setMessage(String message)  { this.message = message; }
+//
+//    public UUID getUserID() { return userID;  }
+//    public void setUserID(UUID userID) { this.userID = userID; }
+//
+//    @Override
+//    public String toString() {
+//        return "Comment{" +
+//                ", commentID=" + commentID +
+//                ", postID=" + parentID +
+//                ", userID=" + userID +
+//                ", comment='" + message + '\'' +
+//                '}';
+//    }
+//}
--- src/main/java/EventSourcing/BasicClasses/CreateCommentCommand.java
+++ src/main/java/EventSourcing/BasicClasses/CreateCommentCommand.java
@@ -1,20 +1,20 @@
-package EventSourcing.BasicClasses;
-
-import org.axonframework.commandhandling.TargetAggregateIdentifier;
-
-import java.util.UUID;
-
-public class CreateCommentCommand {
-    @TargetAggregateIdentifier
-    public final UUID commentID;
-    public UUID parentID;
-    public String message;
-
-    //Called when a comment is created
-    public CreateCommentCommand(UUID commentID, UUID parentID, String message) {
-        this.commentID = commentID;
-        this.parentID = parentID;
-        this.message = message;
-        System.out.println("Inside Constructor CreateCommentCommand(UUID commentID, UUID parentID, String message)");
-    }
-}
+//package EventSourcing.BasicClasses;
+//
+//import org.axonframework.commandhandling.TargetAggregateIdentifier;
+//
+//import java.util.UUID;
+//
+//public class CreateCommentCommand {
+//    @TargetAggregateIdentifier
+//    public final UUID commentID;
+//    public UUID parentID;
+//    public String message;
+//
+//    //Called when a comment is created
+//    public CreateCommentCommand(UUID commentID, UUID parentID, String message) {
+//        this.commentID = commentID;
+//        this.parentID = parentID;
+//        this.message = message;
+//        System.out.println("Inside Constructor CreateCommentCommand(UUID commentID, UUID parentID, String message)");
+//    }
+//}
