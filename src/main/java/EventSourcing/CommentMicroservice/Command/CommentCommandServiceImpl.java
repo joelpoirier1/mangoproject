8d1ba34a25638004a25ada8fe2d0df0a0c88d8c3
Farrukh <farrukh.naghiyev@ucalgary.ca>
2020-03-25 23:39:45 -0600

    -

---
+++ src/main/java/EventSourcing/CommentMicroservice/Command/CommentCommandServiceImpl.java
@@ -0,0 +1,34 @@
+package EventSourcing.CommentMicroservice.Command;
+
+import EventSourcing.BasicClasses.Comment;
+import EventSourcing.BasicClasses.CreateCommentCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/*Implementation of interface "CommentCommandService"*/
+/*A state change within an application starts with a command*/
+@Service
+public class CommentCommandServiceImpl implements CommentCommandService {
+
+    /*Convenience interface provided by Axon used to dispatch commands*/
+    private final CommandGateway commandGateway;
+
+    //This constructor is called immediately when the application is run
+    public CommentCommandServiceImpl(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+        System.out.println("Inside Constructor CommentCommandServiceImpl(CommandGateway commandGateway");
+    }
+
+    /*send() method here will send a command and wait for a response*/
+    @Override
+    public CompletableFuture<String> createComment(Comment comment) {
+        System.out.println("Inside createComment(CommentCreateDTO commentCreateDTO)");
+        /*TODO create local UUID variable*/
+        return commandGateway.send(new CreateCommentCommand(UUID.randomUUID(), UUID.fromString("82525212-6f16-11ea-bc55-0242ac130003"), comment.getMessage()));
+    }
+    /*TODO: Take care of passing the parentID in the above code*/
+}
+
