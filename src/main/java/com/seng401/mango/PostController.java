4c86b3d7c4a49416d05bb9ef984618ee83dcc195
Julia Grab <julia.grab@ucalgary.ca>
2020-03-26 22:41:32 -0600

    Added create comment functionality

---
+++ src/main/java/com/seng401/mango/PostController.java
@@ -0,0 +1,85 @@
+package com.seng401.mango;
+
+import api.CommentCommand;
+import api.CommentRequest;
+import database.repository.PostRepo;
+import database.repository.UserRepo;
+import model.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.UUID;
+
+@Controller
+public class PostController {
+
+    private CommentRequest request = new CommentRequest();
+    private CommentCommand command = new CommentCommand();
+    private UserRepo userRepo = new UserRepo();
+    private PostRepo postRepo = new PostRepo();
+    private UUID currentUser;
+    private UUID currentPost;
+    private RedirectAttributes myRedirect;
+
+    @RequestMapping(value="/post", method = RequestMethod.GET)
+    public String postPage(Model model){
+        if(model.containsAttribute("validate") && model.containsAttribute("postValidate")) {
+            currentUser = (UUID) model.getAttribute("validate");
+            currentPost = (UUID) model.getAttribute("postValidate");
+        }
+
+        if(!model.containsAttribute("validate") && !model.containsAttribute("currentUser"))
+            return "redirect:";
+
+        if(!model.containsAttribute("currentUser")){
+            model.addAttribute("currentUser", userRepo.getUserByID(currentUser));
+        }
+
+        if (!model.containsAttribute("post")){
+            model.addAttribute("post", postRepo.getPostByUUID(currentPost));
+        }
+
+        if(!model.containsAttribute("commentList")){
+            model.addAttribute("commentList", request.getCommentForPostID(currentPost).getComments());
+        }
+
+        return "post";
+    }
+
+    @RequestMapping(value="/addComment", method = RequestMethod.POST)
+    public String addComment(@ModelAttribute("commentForm") CommentForm commentForm, Model model, RedirectAttributes redirectAttributes) {
+        myRedirect = redirectAttributes;
+
+        redirectAttributes.addFlashAttribute("currentUser", userRepo.getUserByID(commentForm.getuID()));
+        redirectAttributes.addFlashAttribute("post", postRepo.getPostByUUID(commentForm.getParentID()));
+
+        if(validateCommentForm(commentForm)){
+            redirectAttributes = myRedirect;
+            return "redirect:/post";
+        }
+
+        command.createComment(commentForm.getuID(), commentForm.getParentID(), commentForm.getComment());
+
+        for(Post p: postRepo.getAllPosts()){
+            if(p.getPostID().compareTo(commentForm.getParentID()) == 0) {
+                p.setCommentList(request.getCommentForPostID(commentForm.getParentID()).getComments());
+                redirectAttributes.addFlashAttribute("commentList", p.getCommentList());
+            }
+        }
+
+        redirectAttributes.addFlashAttribute("commentList", postRepo.getPostByUUID(commentForm.getParentID()).get().getCommentList());
+
+        return "redirect:/post";
+    }
+
+    public boolean validateCommentForm(CommentForm commentForm){
+        if(commentForm.getComment().isEmpty()) {
+            myRedirect.addFlashAttribute("invalidComment", true);
+            return true;
+        } else return false;
+    }
+}
