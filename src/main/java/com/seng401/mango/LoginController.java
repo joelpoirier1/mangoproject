3452ef73f2a040e92c254832bb09a36c9477b038
Julia Grab <julia.grab@ucalgary.ca>
2020-03-26 19:52:29 -0600

    Added login validation and homepage functionalities

--- src/main/java/com/seng401/mango/LoginController.java
+++ src/main/java/com/seng401/mango/LoginController.java
@@ -2,21 +2,27 @@ package com.seng401.mango;
 
 
 import api.CommentRequest;
+import database.repository.PostRepo;
 import database.repository.UserRepo;
 import model.Comment;
+import model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+
+import static model.PostCategory.Lifestyle;
 
 @Controller
 public class LoginController
 {
     private CommentRequest request = new CommentRequest();
     private UserRepo userRepo;
+    private PostRepo postRepo;
     private Model myModel;
 
     //to get login page
@@ -27,20 +33,22 @@ public class LoginController
 
     //checking for login credentials
     @RequestMapping(value="/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute("loginForm") LoginForm loginForm, Model model)
+    public String login(@ModelAttribute("loginForm") LoginForm loginForm, Model model, RedirectAttributes redirectAttributes)
     {
         myModel = model;
         userRepo = new UserRepo();
+        postRepo = new PostRepo();
 
         if(validate(loginForm)) {
             model = myModel;
             return "index";
         }
         else {
-            model = myModel;
-//            model.addAttribute("posts", getComments());
-            model.addAttribute("posts", request.getAllComments().getComments());
-            return "home";
+            redirectAttributes.addFlashAttribute("currentUser", userRepo.getUser(loginForm.getUsername()));
+            redirectAttributes.addFlashAttribute("posts", postRepo.getAllPosts());
+            redirectAttributes.addFlashAttribute("validate", userRepo.getUser(loginForm.getUsername()).get().getId());
+
+            return "redirect:/home";
         }
     }
 
--- src/main/java/com/seng401/mango/PostForm.java
+++ src/main/java/com/seng401/mango/PostForm.java
@@ -3,17 +3,35 @@ package com.seng401.mango;
 import java.util.UUID;
 
 public class PostForm {
-    private String comment;
+    private String title;
+    private String message;
+    private UUID userID;
 
     public PostForm() {
         super();
     }
 
-    public String getComment() {
-        return comment;
+    public UUID getUserID() {
+        return userID;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
